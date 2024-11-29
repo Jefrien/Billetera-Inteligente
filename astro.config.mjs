219f901afb7c30548cc613db536560375f915012
Jefrien Alvizures <jefrienalvizures@gmail.com>
2024-11-29 07:53:40 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel()
+});
